8945ed9beb8db528fced9790068c42bb5ac2d83f
Najaf <najafshaikh1705@gmail.com>
2024-09-13 23:12:05 +0530

    robin

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -14,19 +14,19 @@ function Header() {
             
             {/* Navigation Menu */}
             <ul className='hidden md:flex gap-12 mx-2 items-center'>
-                <li className={`hover:text-tertiary hover:scale-105 cursor-pointer transition-all
+                <li className={`font-bold hover:text-tertiary hover:scale-105 cursor-pointer transition-all
                     ${path === '/dashboard' ? 'font-bold text-tertiary' : ''}`}>
                     Dashboard
                 </li>
-                <li className={`hover:text-tertiary hover:scale-105 cursor-pointer transition-all
+                <li className={`font-bold hover:text-tertiary hover:scale-105 cursor-pointer transition-all
                     ${path === '/questions' ? 'font-bold text-tertiary' : ''}`}>
                     Questions
                 </li>
-                <li className={`hover:text-tertiary hover:scale-105 cursor-pointer transition-all
+                <li className={`font-bold hover:text-tertiary hover:scale-105 cursor-pointer transition-all
                     ${path === '/upgrade' ? 'font-bold text-tertiary' : ''}`}>
                     Upgrade
                 </li>
-                <li className={`hover:text-tertiary hover:scale-105 cursor-pointer transition-all
+                <li className={`font-bold hover:text-tertiary hover:scale-105 cursor-pointer transition-all
                     ${path === '/working' ? 'font-bold text-tertiary' : ''}`}>
                     How it works?
                 </li>
