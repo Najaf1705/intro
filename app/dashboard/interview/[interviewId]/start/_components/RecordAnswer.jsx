391b36a0a6e5ed8c6aa814204496da395bff45e2
Najaf <najafshaikh1705@gmail.com>
2024-11-17 23:29:35 +0530

    sanji

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswer.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswer.jsx
@@ -1,25 +1,63 @@
+"use client"
 import { Button } from '@/components/ui/button';
-import { WebcamIcon } from 'lucide-react';
-import React, { useState } from 'react'
+import { Mic, WebcamIcon } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
 import Webcam from 'react-webcam'
+import useSpeechToText from 'react-hook-speech-to-text';
+
 
 function RecordAnswer() {
+    const [userAnswer,setUserAnswer]=useState('');
+    const [webcamEnabled,setWebcamEnabled]=useState(false);
+
+    const {
+        error,
+        interimResult,
+        isRecording,
+        results,
+        startSpeechToText,
+        stopSpeechToText,
+      } = useSpeechToText({
+        continuous: true,
+        useLegacyResults: false
+      });
+
+      useEffect(() => {
+        results.map((result)=>{
+            setUserAnswer(prevAnswer=>prevAnswer+result?.transcript);
+        })
+      }, [results])
+      
+      
   return (
-    <div className='flex flex-col justify-center items-center'>
-        <div className='flex flex-col items-center'>
-            <WebcamIcon width={200} height={200} className='h-72 w-72 bg-secondary rounded-lg border absolute' />
-            <Webcam
-                mirrored={true}
-                style={{
-                    height:300,
-                    width:'100%',
-                    zIndex:10,
-                    borderRadius:10
-                }}
-            />
+    <div className='flex flex-col justify-center'>
+        <div className='mt-8 flex flex-col items-center'>
+            {webcamEnabled?(
+                <Webcam
+                    onUserMedia={() => setWebcamEnabled(true)}
+                    onUserMediaError={() => setWebcamEnabled(false)}
+                    mirrored={true}
+                    className=' h-72 border rounded-lg'
+                />
+            ):(
+                <WebcamIcon width={'65%'} height={300} className='p-20 bg-secondary rounded-lg border' />
+            )}
         </div>
-        <div className='p-2'>
-            <Button variant="outline" className='my-2 font-bold'>Record Response</Button>
+        <div className='p-2 flex justify-evenly'>
+            <Button variant="outline" className='my-2 font-bold'
+                onClick={isRecording?stopSpeechToText:startSpeechToText}>
+                  {isRecording ? (
+                      <div className="flex items-center gap-2 text-green-500">
+                          <Mic />
+                          <span>Recording...</span>
+                      </div>
+                  ) : (
+                      'Record Response'
+                  )}
+            </Button>
+            <Button onClick={()=>console.log(userAnswer)}>show answer</Button>
+            <Button onClick={() => setWebcamEnabled(!webcamEnabled)} className='my-2 font-bold'>{webcamEnabled?"Turn off webcam":"Turn on webcam"}</Button>
+
         </div>
     </div>
   )
